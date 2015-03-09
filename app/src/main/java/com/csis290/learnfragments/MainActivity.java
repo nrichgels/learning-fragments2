cf5c1bf9bae54db7e3aa4e9bdba5cc0199d7218e
unknown <rl1065uq@BR161-09.mnstate.edu>
2015-03-09 17:32:07 -0500

    adding directory

--- app/src/main/java/com/csis290/learnfragments/MainActivity.java
+++ app/src/main/java/com/csis290/learnfragments/MainActivity.java
@@ -1,7 +1,10 @@
 package com.csis290.learnfragments;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +14,9 @@ public class MainActivity extends Activity {
     private TextView tvGreen;
     private TextView tvBlue;
     private TextView tvRed;
+
+   // Runnable runnable = new Runnable();
+    //Messed up
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 
+                /*
                 Bundle bundle = new Bundle();
                 bundle.putString("key", "Welcome to the Activity");
 
@@ -37,13 +44,17 @@ public class MainActivity extends Activity {
                         .replace(R.id.fragment_container, greenFragment)
                         .addToBackStack("")
                         .commit();
+                        */
             }
         });
 
         tvBlue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new BlueTransactionTask(new Handler()).execute();
 
+
+                /*
                 Bundle bundle = new Bundle();
                 bundle.putString("key2", "Welcome to the Activity, Blue");
 
@@ -55,6 +66,8 @@ public class MainActivity extends Activity {
                         .replace(R.id.fragment_container, blueFragment)
                         .addToBackStack("")
                         .commit();
+                        */
+
             }
         });
 
@@ -69,4 +82,60 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+    // AsyncTask<Params, Progress, Result>
+    // Params
+    class BlueTransactionTask extends AsyncTask<Void, Void, Void>{
+
+        private Handler handler;
+
+        public BlueTransactionTask(Handler handler){
+            this.handler = handler;
+        }
+
+        Runnable blueRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Bundle bundle = new Bundle();
+                bundle.putString("key2", "Welcome to the Activity, Blue");
+
+                BlueFragment blueFragment = new BlueFragment();
+                blueFragment.setArguments(bundle);
+
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, blueFragment)
+                        .setTransition(fragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .addToBackStack("")
+                        .commit();
+            } //End run
+        }; //End Runnable
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d("tag", "onPreExecute()");
+        } //End onPreExecute
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            handler.postDelayed(blueRunnable, 10000);
+            Log.d("tag", "Finished postDelayed().");
+            return null;
+        } //End doInBackground
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d("tag", "onPostExecute");
+        } //End onPostExecute
+    }
+
+    class redTransactionTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+    }//End class
+
 }
